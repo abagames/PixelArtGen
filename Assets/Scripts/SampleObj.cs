a27de9a49e095f538a03e0f81a8afb8ffe89a515
ABA Games <cs8k-cyu@asahi-net.or.jp>
2016-01-03 23:13:29 +0900

    add a sample scene

---
+++ Assets/Scripts/SampleObj.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SampleObj : MonoBehaviour {
+    GameObject pixelArtGenShape;
+
+    void Start () {
+        pixelArtGenShape = Instantiate(Resources.Load("Prefabs/PixelArtGenShape")) as GameObject;
+        pixelArtGenShape.GetComponent<PixelArtGenShape>().
+            Init(gameObject, GetComponent<PixelArtGen>().Generate());
+    }
+	
+	void Update () {
+        var moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        var force = 1000f;
+        var rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(moveDir * force * Time.deltaTime);
+        if (moveDir.sqrMagnitude > 0.1)
+        {
+            float zf = 0;
+            float zAngle = Mathf.SmoothDampAngle
+                (rb.rotation, Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg, ref zf, 0.1f);
+            rb.rotation = zAngle;
+        }
+    }
+}
